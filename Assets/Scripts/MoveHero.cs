c3dbeda2bf60f5be79df49d8b83867dec684d397
Gennadiy <gennadiy13122@gmail.com>
2020-10-30 00:09:16 +0300

    Переработка анимаций, за счет этого произошла очень хорошая оптимизация кода. Анимации перестали глючить. Улучшил физику игры.

--- Assets/Scripts/MoveHero.cs
+++ Assets/Scripts/MoveHero.cs
@@ -4,102 +4,102 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MoveHero : MonoBehaviour
 {
-    static SpriteRenderer SrHero;
-    static Rigidbody2D RbHero;
+    [SerializeField]
+    Rigidbody2D RbHero;
+    [SerializeField]
+    Animator AnimCharacterMove;
     [Header("Движение")]
     public float Speed;
-    public float JumpImpulse;
-    float SpeedGame;   
+    public Transform GroundCheck;
+    float GroundRadius = 0.2f;
+    public LayerMask WhatIsGround;
+    int Move;
+    bool IsHeroRight = true;
     bool IsGrounded;
-    Animator AnimCharacterMove;
+    [Header("Атака")]
+    bool IsReadyAttackHero = true;
+    public GameObject AttackPositionRight;
+    public GameObject AttackPositionLeft;
     [Header("Монеты")]
     public Text CoinT;
     int Coin;
-    int LifePoints=15;
-    public GameObject AttackPositionRight;
-    public GameObject AttackPositionLeft;
+    int LifePoints = 100;
+    [Header("Лестницы")]
     bool IsStairs = false;
     bool IsStairsGo = false;
     void Start()
     {
-        SrHero = GetComponent<SpriteRenderer>();
-        RbHero = GetComponent<Rigidbody2D>();
-        AnimCharacterMove = GetComponent<Animator>();
         Coin = PlayerPrefs.GetInt("Coin");
         CoinT.text = Coin.ToString();
     }
     void FixedUpdate()
     {
+        IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundRadius, WhatIsGround);
+        AnimCharacterMove.SetBool("Ground", IsGrounded);
+        AnimCharacterMove.SetFloat("vSpeed", RbHero.velocity.y);
+        AnimCharacterMove.SetFloat("Speed", Mathf.Abs(Move));
         if (IsStairs && IsStairsGo)
         {
-            RbHero.velocity = new Vector2(0, 10);
+            RbHero.velocity = new Vector2(0, Speed * Time.fixedDeltaTime);
         }
-        RbHero.velocity = new Vector2(SpeedGame, RbHero.velocity.y);
+        RbHero.velocity = new Vector2(Move * Speed, RbHero.velocity.y);
+        if (Move > 0 && !IsHeroRight)
+        {
+            Flip();
+        }
+        //обратная ситуация. отражаем персонажа влево
+        else if (Move < 0 && IsHeroRight)
+        {
+            Flip();
+        }
         if (LifePoints == 0)
         {
             Dead();
         }
     }
+    private void Flip()
+    {
+        IsHeroRight = !IsHeroRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
     public void MoveRightHero()
     {
-        SpeedGame = Speed;
-        AnimationFalse();
-        AnimCharacterMove.SetBool("MoveRight", true); //анимация 
+        Move = 1;
     }
     public void MoveLeftHero()
     {
-        SpeedGame = -Speed;
-        AnimationFalse();
-        AnimCharacterMove.SetBool("MoveLeft", true);
+        Move = -1;
     }
-    public void MoveStopHero()
+    public void MoveHeroUp()
     {
-        if (SrHero.flipX == true)
-        {
-            AnimationFalse();
-            AnimCharacterMove.SetBool("StayLeft", true);//анимация 
-        }
-        else if (SrHero.flipX == false)
-        {
-            AnimationFalse();
-            AnimCharacterMove.SetBool("StayRight", true);//анимация 
-        }     
-        SpeedGame = 0;
+        Move = 0;
     }
     public void MoveJumpHero()
     {
         IsStairsGo = true;
-        if (IsGrounded && !IsStairs)
-        {          
-            AnimationFalse();
-            //проверка какую анимацию проигрывать
-            if (SrHero.flipX == true)
-            {
-                AnimCharacterMove.SetBool("JumpLeft", true);
-            }
-            else if (SrHero.flipX == false)
-            {
-                AnimCharacterMove.SetBool("JumpRight", true);
-            }
-            RbHero.AddForce(new Vector2(0, JumpImpulse));
-            IsStairsGo = false;
+        if (IsGrounded)
+        {
+            //устанавливаем в аниматоре переменную в false
+            AnimCharacterMove.SetBool("Ground", false);
+            //прикладываем силу вверх, чтобы персонаж подпрыгнул
+            RbHero.AddForce(new Vector2(0, 600));
         }
+        IsStairsGo = false;
     }
-    void AnimationFalse()
+    public void IsStairsV()
     {
-        AnimCharacterMove.SetBool("JumpLeft", false);
-        AnimCharacterMove.SetBool("JumpRight", false);
-        AnimCharacterMove.SetBool("MoveLeft", false); //анимация 
-        AnimCharacterMove.SetBool("MoveRight", false);
-        AnimCharacterMove.SetBool("StayLeft", false);
-        AnimCharacterMove.SetBool("StayRight", false);
-        AnimCharacterMove.SetBool("AttackLeft", false);
-        AnimCharacterMove.SetBool("AttackRight", false);
         IsStairsGo = false;
     }
     public void HeroAttack()
-    {    
-        StartCoroutine(DamageWait());       
+    {
+        if (IsReadyAttackHero)
+        {
+            AnimCharacterMove.SetBool("Attack", true);
+            IsReadyAttackHero = false;
+            StartCoroutine(DamageWait());
+        }
     }
     public void Damage(int dmg)
     {
@@ -111,28 +111,23 @@ public class MoveHero : MonoBehaviour
     }
     IEnumerator DamageWait()
     {
-        AnimationFalse();
-        if (SrHero.flipX == true)
+        if (!IsHeroRight)
         {
             AttackPositionLeft.SetActive(true);
-            AnimCharacterMove.SetBool("AttackLeft", true);
         }
-        else if (SrHero.flipX == false)
+        else if (IsHeroRight)
         {
             AttackPositionRight.SetActive(true);
-            AnimCharacterMove.SetBool("AttackRight", true);
         }
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.35f);
         AttackPositionRight.SetActive(false);
-        AttackPositionLeft.SetActive(false); 
+        AttackPositionLeft.SetActive(false);
+        AnimCharacterMove.SetBool("Attack", false);
+        IsReadyAttackHero = true;
     }
     //проверка для прыжка
     private void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Ground")
-        {
-            IsGrounded = true;
-        }
         if (coll.gameObject.tag == "Coin")
         {
             Coin++;
@@ -140,16 +135,9 @@ public class MoveHero : MonoBehaviour
             Destroy(coll.gameObject);
         }
     }
-    private void OnCollisionExit2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "Ground")
-        {
-            IsGrounded = false;
-        }
-    }
     //сбор монеток
     private void OnTriggerEnter2D(Collider2D coll)
-    {       
+    {
         if (coll.gameObject.CompareTag("EnemyAttack"))
         {
             Damage(1);
@@ -161,6 +149,7 @@ public class MoveHero : MonoBehaviour
         if (coll.gameObject.tag == "Ladder")
         {
             IsStairs = true;
+            RbHero.gravityScale = 0.1f;
         }
     }
     private void OnTriggerExit2D(Collider2D coll)
@@ -168,6 +157,7 @@ public class MoveHero : MonoBehaviour
         if (coll.gameObject.tag == "Ladder")
         {
             IsStairs = false;
+            RbHero.gravityScale = 1f;
         }
     }
     void Dead()
